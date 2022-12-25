5799cfcc84a3f016286bf17b9a71c5c53e0fa120
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-12-24 22:48:29 -0300

    Accordeon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,10 @@
 const itemDT = document.querySelectorAll('dt')
-const itemDD = document.querySelectorAll('dd')
 
-itemDT.forEach((item, index)=>{
-    item.addEventListener('click', ()=>{
-        item.classList.toggle('active')
-        vinculaDD(index)
-    })
-})
-
-function vinculaDD(index){
-    itemDD.forEach((item)=>{
-        item.classList.toggle('ativo')
-    })
+function activeAccordion(){
+    this.classList.toggle('active')  
+    this.nextElementSibling.classList.toggle('ativo')
 }
+
+itemDT.forEach((item)=>{
+    item.addEventListener('click', activeAccordion)
+})
